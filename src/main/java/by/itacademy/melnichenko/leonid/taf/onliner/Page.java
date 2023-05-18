34a18f1bef5d04000e33de16e27a0d787b09c615
LeonidMelnichenko <122801542+LeonidMelnichenko@users.noreply.github.com>
2023-05-18 21:44:04 +0300

    add first commit

--- src/main/java/by/itacademy/melnichenko/leonid/taf/onliner/Page.java
+++ src/main/java/by/itacademy/melnichenko/leonid/taf/onliner/Page.java
@@ -1,2 +1,8 @@
-package by.itacademy.melnichenko.leonid.taf.onliner;public class Page {
+package by.itacademy.melnichenko.leonid.taf.onliner;
+
+public class Page {
+    String buttonLoginXpath = "//*[@id='userbar']/div[1]/div/div/div[1]";
+    String inputLoginXpath = "//*[@id='auth-container']/div/div[2]/div/form/div[1]/div/div[2]/div/div/div/div/input";
+    String inputPasswordXpath = "//*[@id='auth-container']/div/div[2]/div/form/div[2]/div/div/div/div/input";
+    String buttonSubmitXpath = "//*[@id='auth-container']/div/div[2]/div/form/div[3]/button";
 }
--- src/test/java/by/itacademy/melnichenko/leonid/taf/onliner/PageTest.java
+++ src/test/java/by/itacademy/melnichenko/leonid/taf/onliner/PageTest.java
@@ -1,2 +1,20 @@
-package by.itacademy.melnichenko.leonid.taf.onliner;public class PageTest {
+package by.itacademy.melnichenko.leonid.taf.onliner;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class PageTest {
+    @Test
+    public void testLoginForm(){
+        Page page = new Page();
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://www.onliner.by/");
+        WebElement buttonLogin = driver.findElement(By.xpath(page.buttonLoginXpath));
+        buttonLogin.click();
+
+        WebElement inputLogin = driver.findElement(By.xpath(page.inputLoginXpath));
+        inputLogin.sendKeys("mail@mail.com");
+
+    }
 }
